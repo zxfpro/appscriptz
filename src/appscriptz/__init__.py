21df2a00538032fa608bfd0f5f0201cef16bb243
823042332@qq.com <823042332@qq.com>
2025-06-26 18:34:18 +0800

    2025年06月26日18:34:16

--- src/appscriptz/__init__.py
+++ src/appscriptz/__init__.py
@@ -2,3 +2,7 @@ from .scripts.applescript import Notes,Calulate,Reminder
 from .scripts.aifunc import generate_schedule
 
 from .main import APPManager
+
+from querypipz.log import Log
+
+Log.reset_level('debug')
